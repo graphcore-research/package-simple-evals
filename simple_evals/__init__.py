a7f8521a8f1d179cebdca3f1b3c2fde4a24de0a4
Alexandre Payot <alexandrep@graphcore.ai>
2025-03-17 14:40:33 +0000

    Add pre-commit and tests

--- simple_evals/__init__.py
+++ simple_evals/__init__.py
@@ -5,18 +5,18 @@ simple_evals - A lightweight library for evaluating language models.
 __version__ = "0.1.0"
 
 # Import key modules and functions
-from simple_evals.simple_evals import main
-from simple_evals.common import *
-from simple_evals.types import *
+from simple_evals.simple_evals import main  # noqa: F401
+from simple_evals.common import *  # noqa: F403
+from simple_evals.types import *  # noqa: F403
 
 # Import all evaluation classes
-from simple_evals.simpleqa_eval import SimpleQAEval
-from simple_evals.mmlu_eval import MMLUEval
-from simple_evals.mgsm_eval import MGSMEval
-from simple_evals.math_eval import MathEval
-from simple_evals.humaneval_eval import HumanEval
-from simple_evals.gpqa_eval import GPQAEval
-from simple_evals.drop_eval import DropEval
+from simple_evals.simpleqa_eval import SimpleQAEval  # noqa: F401
+from simple_evals.mmlu_eval import MMLUEval  # noqa: F401
+from simple_evals.mgsm_eval import MGSMEval  # noqa: F401
+from simple_evals.math_eval import MathEval  # noqa: F401
+from simple_evals.humaneval_eval import HumanEval  # noqa: F401
+from simple_evals.gpqa_eval import GPQAEval  # noqa: F401
+from simple_evals.drop_eval import DropEval  # noqa: F401
 
 # Import samplers
-from simple_evals.sampler import * 
+from simple_evals.sampler import *  # noqa: F403
--- simple_evals/sampler/__init__.py
+++ simple_evals/sampler/__init__.py
@@ -2,6 +2,6 @@
 simple_evals.sampler - Sampling modules for language model evaluation.
 """
 
-from simple_evals.sampler.chat_completion_sampler import ChatCompletionSampler
-from simple_evals.sampler.o_chat_completion_sampler import OChatCompletionSampler
-from simple_evals.sampler.claude_sampler import ClaudeCompletionSampler 
+from simple_evals.sampler.chat_completion_sampler import ChatCompletionSampler  # noqa: F401
+from simple_evals.sampler.o_chat_completion_sampler import OChatCompletionSampler  # noqa: F401
+from simple_evals.sampler.claude_sampler import ClaudeCompletionSampler  # noqa: F401
